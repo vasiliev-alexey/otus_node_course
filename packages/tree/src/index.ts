d02fed6777339c0fafd3f2e802c2745b6f2d1054
vasiliev-alexey <alex_v81@mail.ru>
2022-01-20 18:48:53 +0300

    fix pr comments

--- packages/tree/src/index.ts
+++ packages/tree/src/index.ts
@@ -14,11 +14,7 @@ program
   .argument("<root directory>", "root directory")
   .option("-d, --depth <command>", "structure display depth", "2")
   .action(async (root, options) => {
-    // const dir = Path.basename(Path.resolve(root));
-
-    //console.log("dir", dir);
     const files = await walk(Path.normalize(root), options.depth);
-    // console.log("files", files);
     printer(files);
   });
 
