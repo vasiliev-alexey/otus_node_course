76f7898e4726fa196482987f2330b851725fbe96
vasiliev-alexey <alex_v81@MAIL.RU>
2022-01-21 15:39:10 +0300

    logger config

--- packages/streams/src/services/logger.ts
+++ packages/streams/src/services/logger.ts
@@ -1,3 +1,11 @@
-import { Logger } from "tslog";
+import { Logger, TLogLevelName } from "tslog"; // eslint-disable-line import/named
 
-export const rootLogger: Logger = new Logger({ name: "Root Logger" });
+import { config } from "dotenv";
+config();
+
+console.log("process.env.DEBUG_LEVEL", process.env.DEBUG_LEVEL);
+
+export const rootLogger: Logger = new Logger({
+  name: "Root Logger",
+  minLevel: (process.env.DEBUG_LEVEL as TLogLevelName) || "info",
+});
