abbc353e71c895a25478a6c5859d1b6dab066ffc
vasiliev-alexey <alex_v81@mail.ru>
2022-03-07 19:01:31 +0300

    add ws hw

--- packages/streams/src/services/logger.ts
+++ packages/streams/src/services/logger.ts
@@ -2,8 +2,6 @@ import { config } from "dotenv";
 import { Logger, TLogLevelName } from "tslog"; // eslint-disable-line import/named
 config();
 
-console.log("process.env.DEBUG_LEVEL", process.env.DEBUG_LEVEL);
-
 export const rootLogger: Logger = new Logger({
   name: "Root Logger",
   minLevel: (process.env.DEBUG_LEVEL as TLogLevelName) || "info",
